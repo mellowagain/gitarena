fbdff7d1a08ba9947a8c8d77cf632d422e94a5c7
Marc Steiner <git@marcsteiner.me>
2021-06-26 22:06:15 +0200

    Change method in routes init as well

--- src/routes/user/mod.rs
+++ src/routes/user/mod.rs
@@ -3,5 +3,5 @@ use actix_web::web::ServiceConfig;
 pub(crate) mod user_create;
 
 pub(crate) fn init(config: &mut ServiceConfig) {
-    config.service(user_create::register);
+    config.service(user_create::handle_post);
 }
