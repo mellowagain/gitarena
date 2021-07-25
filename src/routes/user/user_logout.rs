5d9f5c7078ab4792886cffdedfa7d704051d9e0f
Mari <git@cutegirl.tech>
2021-07-25 02:49:06 +0200

    Remove unused imports

--- src/routes/user/user_logout.rs
+++ src/routes/user/user_logout.rs
@@ -2,10 +2,9 @@ use crate::error::GAErrors::HttpError;
 
 use actix_identity::Identity;
 use actix_web::http::header;
-use actix_web::{HttpResponse, Responder, web};
+use actix_web::{HttpResponse, Responder};
 use anyhow::Result;
 use gitarena_macros::route;
-use sqlx::PgPool;
 
 #[route("/logout", method="POST")]
 pub(crate) async fn logout(id: Identity) -> Result<impl Responder> {
