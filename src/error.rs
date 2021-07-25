f03615f351a6ea42b4d04d34f2751092605a7a69
Mari <git@cutegirl.tech>
2021-07-25 03:46:27 +0200

    Implement new error type Git

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use actix_web::error::ResponseError;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use anyhow::Error as AnyhowError;
-use log::error;
+use log::{error, warn};
 use serde::ser::SerializeStruct;
 use serde::{Serialize, Serializer};
 use serde_json::json;
@@ -14,7 +14,10 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub(crate) enum GAErrors {
     #[error("{1}")]
-    HttpError(u16, String)
+    HttpError(u16, String),
+
+    #[error("(null)")]
+    GitError(u16, Option<String>)
 }
 
 pub(crate) struct GitArenaError {
@@ -56,32 +59,41 @@ impl ResponseError for GitArenaError {
     fn status_code(&self) -> StatusCode {
         if let Some(e) = self.error.downcast_ref::<GAErrors>() {
             match e {
-                GAErrors::HttpError(status_code, _) => StatusCode::from_u16(*status_code)
-            }.unwrap_or(StatusCode::IM_A_TEAPOT) // A programmer passed a invalid status code
+                GAErrors::HttpError(status_code, _) => StatusCode::from_u16(*status_code),
+                GAErrors::GitError(status_code, _) => StatusCode::from_u16(*status_code)
+            }.unwrap_or_else(|error| {
+                warn!("Invalid status code passed to GitArena error: {}", error);
+                StatusCode::IM_A_TEAPOT
+            }) // A programmer passed a invalid status code
         } else {
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
 
     fn error_response(&self) -> HttpResponse {
-        let message = if let Some(e) = self.error.downcast_ref::<GAErrors>() {
-            match e {
-                GAErrors::HttpError(_, message) => message
+        if let Some(gitarena_error) = self.error.downcast_ref::<GAErrors>() {
+            return match gitarena_error {
+                GAErrors::HttpError(_, message) => {
+                    HttpResponseBuilder::new(self.status_code())
+                        .json(json!({
+                            "error": message
+                        }))
+                }
+                GAErrors::GitError(_, message_option) => {
+                    let mut response = HttpResponseBuilder::new(self.status_code());
+
+                    if let Some(message) = message_option {
+                        return response.body(message);
+                    }
+
+                    response.finish()
+                }
             }
-        } else {
-            "Internal server error occurred"
-        };
-
-        let status_code = self.status_code();
-
-        if status_code.is_server_error() {
-            error!("Error occurred while handling route: {}", self.error.root_cause())
         }
 
-        let json = json!({
-            "error": format!("{}", message)
-        });
-
-        HttpResponseBuilder::new(status_code).json(json)
+        HttpResponse::InternalServerError()
+            .json(json!({
+                "error": "Internal server error occurred"
+            }))
     }
 }
