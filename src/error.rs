767c0c3b2134e9877d7faf70e94847c4ba99a599
Mari <git@cutegirl.tech>
2021-12-31 03:22:37 +0100

    Overhaul error file
    * New macro `die!`
    * Responses based on pre-defined type (html, json, git etc...)

--- src/error.rs
+++ src/error.rs
@@ -1,156 +1,176 @@
 use crate::git::io::band::Band;
 use crate::git::io::writer::GitWriter;
+use crate::templates;
 
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 use actix_web::dev::HttpResponseBuilder;
-use actix_web::error::ResponseError;
+use actix_web::error::{InternalError, PrivateHelper};
 use actix_web::http::StatusCode;
-use actix_web::HttpResponse;
-use anyhow::Error as AnyhowError;
-use anyhow::Result as AnyhowResult;
+use actix_web::{HttpResponse, ResponseError};
+use anyhow::{Error, Result};
 use async_compat::Compat;
+use derive_more::{Display, Error};
 use futures::executor;
 use log::error;
-use serde::ser::SerializeStruct;
-use serde::{Serialize, Serializer};
 use serde_json::json;
-use thiserror::Error;
+use tera::Context;
 
-#[derive(Error, Debug)]
-pub(crate) enum GAErrors {
-    #[error("{1}")]
-    HttpError(u16, String),
+macro_rules! die {
+    ($code:expr) => {
+        return Err($crate::error::WithStatusCode::new(actix_web::http::StatusCode::$code).into());
+    }
+    ($code:literal) => {{
+        use anyhow::Context as _;
 
-    #[error("{1}")]
-    PlainError(u16, String),
-
-    #[error("(null)")]
-    GitError(u16, Option<String>),
-
-    #[error("Unable to parse {0} from `{1}`")]
-    ParseError(&'static str, String),
-
-    #[error("Unable to unpack {0} for pack")]
-    PackUnpackError(&'static str),
-
-    #[error("Error occurred when trying to run hook: {0}")]
-    HookError(&'static str),
-
-    #[error("Type constraint was violated on {0}")]
-    TypeConstraintViolated(&'static str),
-
-    #[error("Not authenticated. Try logging in")]
-    NotAuthenticated
+        return Err($crate::error::WithStatusCode::try_new($code).context("Tried to die with invalid status code")?.into());
+    }}
+    ($code:expr, $message:literal) => {
+        return Err($crate::error::WithStatusCode {
+            code: actix_web::http::StatusCode::$code,
+            source: anyhow::anyhow!($message),
+            display: false
+        })
+    }
+    ($err:expr $(,)?) => ({
+        return Err($crate::error::WithStatusCode {
+            code: actix_web::http::StatusCode::$code,
+            source: anyhow::anyhow!($err),
+            display: false
+        })
+    })
+    ($code:expr, $fmt:literal, $($arg:tt)*) => {
+        return Err($crate::error::WithStatusCode {
+            code: actix_web::http::StatusCode::$code,
+            source: anyhow::anyhow!($fmt, $($arg)*),
+            display: true
+        })
+    }
 }
 
-impl From<GAErrors> for GitArenaError {
-    fn from(ga_error: GAErrors) -> Self {
-        GitArenaError {
-            error: ga_error.into()
+#[derive(Debug, Display, Error)]
+#[display("http status {} caused by {}", code, source)]
+pub(crate) struct WithStatusCode {
+    code: StatusCode,
+    source: Option<Error>,
+    display: bool // Whenever cause() should be shown to the user
+}
+
+impl WithStatusCode {
+    pub(crate) fn new(code: StatusCode) -> WithStatusCode {
+        WithStatusCode {
+            code,
+            source: None,
+            display: false
         }
     }
-}
 
-pub(crate) struct GitArenaError {
-    error: AnyhowError
-}
-
-impl Display for GitArenaError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "{}", self.error)
+    pub(crate) fn try_new(code: u16) -> Result<WithStatusCode> {
+        Ok(WithStatusCode {
+            code: StatusCode::from_u16(code)?,
+            source: None,
+            display: false
+        })
     }
+}
+
+#[derive(Clone)]
+pub(crate) struct GitArenaError {
+    source: Error,
+    display_type: ErrorDisplayType
 }
 
 impl Debug for GitArenaError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "{:?}", self.error)
+        Debug::fmt(&self.source, f)
     }
 }
 
-impl Serialize for GitArenaError {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-    {
-        let cause = format!("{}", self.error);
-
-        let mut state = serializer.serialize_struct("GitArenaError", 1)?;
-        state.serialize_field("error", cause.as_str())?;
-        state.end()
-    }
-}
-
-impl From<AnyhowError> for GitArenaError {
-    fn from(error: AnyhowError) -> Self {
-        GitArenaError { error }
+impl Display for GitArenaError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Display::fmt(&self.source, f)
     }
 }
 
 impl ResponseError for GitArenaError {
     fn status_code(&self) -> StatusCode {
-        if let Some(e) = self.error.downcast_ref::<GAErrors>() {
-            match e {
-                GAErrors::HttpError(status_code, _) => StatusCode::from_u16(*status_code),
-                GAErrors::PlainError(status_code, _) => StatusCode::from_u16(*status_code),
-                GAErrors::GitError(status_code, _) => StatusCode::from_u16(*status_code),
-                GAErrors::NotAuthenticated => Ok(StatusCode::UNAUTHORIZED),
-
-                _ => Ok(StatusCode::INTERNAL_SERVER_ERROR)
-            }.unwrap_or_else(|error| {
-                panic!("Invalid status code passed to GitArena error: {}", error);
-            })
-        } else {
-            StatusCode::INTERNAL_SERVER_ERROR
+        match self.source.downcast_ref::<WithStatusCode>() {
+            Some(with_code) => with_code.code,
+            None => StatusCode::INTERNAL_SERVER_ERROR
         }
     }
 
     fn error_response(&self) -> HttpResponse {
-        if let Some(gitarena_error) = self.error.downcast_ref::<GAErrors>() {
-            return match gitarena_error {
-                GAErrors::HttpError(_, message) => {
-                    HttpResponseBuilder::new(self.status_code())
-                        .json(json!({
-                            "error": message
-                        }))
-                }
-                GAErrors::PlainError(_, message) => HttpResponseBuilder::new(self.status_code()).body(message),
-                GAErrors::GitError(_, message_option) => {
-                    match message_option {
-                        Some(message) => {
-                            // TODO: Refactor this to no longer block the whole thread
-                            let response: AnyhowResult<HttpResponse> = executor::block_on(Compat::new(async {
-                                let mut writer = GitWriter::new();
-                                writer.write_text_sideband(Band::Error, format!("error: {}", message)).await?;
+        let mut builder = HttpResponseBuilder::new(self.status_code());
 
-                                let response = writer.serialize().await?;
+        let display = self.source.downcast_ref::<WithStatusCode>().map_or_else(|| false, |w| w.display);
+        let message = if display {
+            self.source.to_string()
+        } else {
+            self.status_code().canonical_reason().unwrap_or_default().to_string()
+        };
 
-                                // Git doesn't show client errors if the response isn't 200 for some reason
-                                Ok(HttpResponse::Ok().body(response))
-                            }));
+        match &self.display_type {
+            ErrorDisplayType::Html | ErrorDisplayType::Git => {
+                // TODO: Refactor this to no longer block the whole thread
+                executor::block_on(Compat::new(async {
+                    render_error_async(&self, builder, message.as_str()).await
+                }))
+            },
+            ErrorDisplayType::Htmx(inner) => {
+                // TODO: Send partial htmx instead
+                let mut error = self.clone();
+                error.display_type = **inner;
 
-                            response.unwrap_or_else(|err| {
-                                error!("In addition, another error occurred while handling the previous error: {}", err);
-                                HttpResponse::InternalServerError().finish()
-                            })
-                        }
-                        None => HttpResponseBuilder::new(self.status_code()).finish()
-                    }
-                },
-                GAErrors::NotAuthenticated => {
-                    HttpResponse::Unauthorized()
-                        .json(json!({
-                            "error": "Not logged in"
-                        }))
-                },
-
-                _ => HttpResponse::InternalServerError().finish()
+                error.error_response()
             }
+            ErrorDisplayType::Json => builder.json(json!({
+                "error": message
+            })),
+            ErrorDisplayType::Plain => builder.body(message)
         }
-
-        HttpResponse::InternalServerError()
-            .json(json!({
-                "error": "Internal server error occurred"
-            }))
     }
+}
+
+async fn render_error_async(renderer: &GitArenaError, builder: HttpResponseBuilder, message: &str) -> HttpResponse {
+    match renderer.display_type {
+        ErrorDisplayType::Html => render_html_error(renderer.status_code(), message).await,
+        ErrorDisplayType::Git => render_git_error(message).await,
+        _ => unreachable!("Only html and git errors require async handling")
+    }.unwrap_or_else(|err| {
+        error!("In addition, another error occurred while handling the previous error: {}", err);
+        InternalError::new(err, StatusCode::INTERNAL_SERVER_ERROR).into()
+    })
+}
+
+async fn render_html_error(code: StatusCode, message: &str, display: bool) -> Result<HttpResponse> {
+    let mut context = Context::new();
+    context.try_insert("error", message)?;
+
+    if cfg!(debug_assertions) {
+        context.try_insert("debug", &true)?;
+    }
+
+    let template_name = format!("error/{}.html", code.as_u16());
+    let template = templates::TERA.read().await.render(template_name.as_str(), &context)?;
+
+    Ok(HttpResponseBuilder::new(code).body(template))
+}
+
+async fn render_git_error(message: &str) -> Result<HttpResponse> {
+    let mut writer = GitWriter::new();
+    writer.write_text_sideband(Band::Error, format!("error: {}", message)).await?;
+
+    let response = writer.serialize().await?;
+
+    // Git doesn't show client errors if the response isn't 200 for some reason
+    Ok(HttpResponse::Ok().body(response))
+}
+
+pub(crate) enum ErrorDisplayType {
+    Html,
+    Htmx(Box<ErrorDisplayType>),
+    Json,
+    Git,
+    Plain
 }
