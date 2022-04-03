6a0f17d55a6b98035c351e9ba381fc603ad3943e
Mari <git@cutegirl.tech>
2022-04-04 00:20:01 +0200

    Add list of all packets into `mod.rs`

--- gitarena-common/src/packets/mod.rs
+++ gitarena-common/src/packets/mod.rs
@@ -1,6 +1,17 @@
+use num_derive::{FromPrimitive, ToPrimitive};
+
 pub mod git; // 1xxx
 
 #[repr(usize)]
 pub enum PacketCategory {
     Git = 1000
+}
+
+// TODO: Find a way to automatically generate this
+// Would be able to do this now if proc macros have state
+
+#[repr(usize)]
+#[derive(FromPrimitive, ToPrimitive)]
+pub enum PacketId {
+    GitImport = 1001
 }
