5f53384faf05f5bf4cda5f7071580668f72bdb1f
Mari <git@cutegirl.tech>
2022-04-04 00:20:01 +0200

    Add list of all packets into `mod.rs`

--- gitarena-common/src/packets/git.rs
+++ gitarena-common/src/packets/git.rs
@@ -1,8 +1,8 @@
 use serde::{Deserialize, Serialize};
 use gitarena_macros::IpcPacket;
 
-#[derive(Deserialize, Serialize, Debug, IpcPacket)]
-#[ipc(packet = "Git", id = 1)]
+#[derive(Deserialize, Serialize, Debug, Default, IpcPacket)]
+#[ipc(packet = "Git", id = 1)] // = 1001
 pub struct GitImport {
     pub url: String,
     pub username: Option<String>,
