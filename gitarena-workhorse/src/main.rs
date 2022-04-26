ef6f68f91be092f749fb77ec323a27eb99213825
Mari <git@cutegirl.tech>
2022-04-26 10:49:18 +0200

    Prepare workhorse for incoming connection handling

--- gitarena-workhorse/src/main.rs
+++ gitarena-workhorse/src/main.rs
@@ -1,99 +1,63 @@
-use std::env::VarError;
-use std::error::Error;
-use std::path::Path;
-use std::{env, fs, io};
+use std::io;
 
 use anyhow::{Context, Result};
-use log::info;
-use tracing_appender::non_blocking::WorkerGuard;
-use tracing_appender::rolling;
-use tracing_subscriber::filter::LevelFilter;
-use tracing_subscriber::fmt::Layer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{EnvFilter, Registry};
+use futures::stream::StreamExt;
+use gitarena_common::ipc::{ipc_path, IpcPacket};
+use gitarena_common::log::init_logger;
+use gitarena_common::num_traits::cast::FromPrimitive;
+use gitarena_common::packets::git::GitImport;
+use gitarena_common::packets::PacketId;
+use log::{debug, error, info, warn};
+use parity_tokio_ipc::Endpoint;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite};
 use tracing_unwrap::ResultExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _log_guards = init_logger()?;
+    let _log_guards = init_logger("gitarena-workhorse", &[])?;
+
+    Endpoint::new(ipc_path()?.to_owned())
+        .incoming()
+        .with_context(|| format!("Failed to create endpoint at {}", ipc_path().unwrap_or_log()))? // .unwrap_or_log() is safe as it would've excited early two lines above if this errors
+        .for_each(|connection| async {
+            if let Err(err) = handle(connection).await {
+                error!("Error occurred while reading stream: {}", err);
+            }
+        })
+        .await;
+
+    info!("Thank you and goodbye.");
 
     Ok(())
 }
 
-// TODO: Move this to gitarena-common as this will be commonly shared between all gitarena crates
-fn init_logger() -> Result<Vec<WorkerGuard>> {
-    let mut env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|err| {
-        let not_found = err.source()
-            .map(|o| o.downcast_ref::<VarError>().map_or_else(|| false, |err| matches!(err, VarError::NotPresent)))
-            .unwrap_or(false);
+async fn handle<T: AsyncRead + AsyncWrite + Unpin + 'static>(connection: Result<T, io::Error>) -> Result<()> {
+    let mut connection = connection?;
 
-        if !not_found {
-            eprintln!("Warning: Unable to parse `{}` environment variable, using default values: {}", EnvFilter::DEFAULT_ENV, err);
+    let type_ = connection.read_u64().await.context("Failed to read type")?;
+    let length = connection.read_u64().await.context("Failed to read length")?;
+
+    let id: PacketId = PacketId::from_u64(type_).with_context(|| format!("Received unknown packet id: {}", type_))?;
+
+    let mut buffer: Vec<u8> = Vec::with_capacity(length as usize);
+    let read_length = connection.read(buffer.as_mut_slice()).await.context("Failed to read payload")? as u64;
+
+    if read_length != length {
+        warn!("Failed to read correct payload size, expected: {} read: {}", length, read_length);
+    }
+
+    // Bincode is configured in gitarena-common/src/ipc.rs to use little endianness
+    let ser_type = &type_.to_le_bytes();
+    let ser_length = &length.to_le_bytes();
+    let payload = &[ser_type, ser_length, buffer.as_slice()].concat();
+
+    match id {
+        PacketId::GitImport => {
+            let packet = IpcPacket::<GitImport>::deserialize(payload)?;
+
+            debug!("got {:?}", packet);
         }
+    }
 
-        let level = if cfg!(debug_assertions) {
-            LevelFilter::DEBUG
-        } else {
-            LevelFilter::INFO
-        };
-
-        EnvFilter::default()
-            .add_directive(level.into())
-    });
-
-    let mut guards = Vec::<WorkerGuard>::with_capacity(1);
-
-    // In debug mode we only write to stdout, in production to a both stdout (pretty) and file (json)
-    let stdout_log = {
-        let (writer, guard) = tracing_appender::non_blocking(io::stdout());
-
-        let layer = Layer::new()
-            .with_thread_ids(true)
-            .with_writer(writer);
-
-        guards.push(guard);
-        layer
-    };
-
-    let file_log = if cfg!(debug_assertions) || env::var_os("DEBUG_FILE_LOG").is_some() {
-        let logs_dir = Path::new("logs");
-
-        if !logs_dir.exists() {
-            fs::create_dir_all(logs_dir)?;
-        }
-
-        let appender = rolling::daily("logs", "gitarena-workhorse.log");
-        let (writer, guard) = tracing_appender::non_blocking(appender);
-
-        let layer = Layer::new()
-            .with_thread_ids(true)
-            .with_writer(writer)
-            .json();
-
-        guards.push(guard);
-        Some(layer)
-    } else {
-        None
-    };
-
-    let tokio_console = if cfg!(tokio_unstable) {
-        env_filter = env_filter.add_directive("tokio=trace".parse().unwrap_or_log())
-            .add_directive("runtime=trace".parse().unwrap_or_log());
-
-        Some(console_subscriber::spawn())
-    } else {
-        None
-    };
-
-    // https://stackoverflow.com/a/66138267
-    Registry::default()
-        .with(env_filter)
-        .with(stdout_log)
-        .with(file_log)
-        .with(tokio_console)
-        .try_init()
-        .context("Failed to initialize logger")?;
-
-    Ok(guards)
+    Ok(())
 }
