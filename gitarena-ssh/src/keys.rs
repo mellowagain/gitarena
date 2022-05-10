ad1307e050d9ad9ffc7709e6c0374cf1476581fa
Mari <git@cutegirl.tech>
2022-05-11 01:01:15 +0200

    Sort imports in `keys.rs`

--- gitarena-ssh/src/keys.rs
+++ gitarena-ssh/src/keys.rs
@@ -1,8 +1,8 @@
-use gitarena_common::prelude::*;
-use gitarena_common::database::Database;
-use sqlx::Executor;
 use anyhow::Result;
+use gitarena_common::database::Database;
 use gitarena_common::database::models::KeyType;
+use gitarena_common::prelude::*;
+use sqlx::Executor;
 
 pub(crate) async fn print_all<'e, E: Executor<'e, Database = Database>>(executor: E) -> Result<()> {
     let keys: Vec<(KeyType, Vec<u8>)> = sqlx::query_as("select algorithm, key from ssh_keys where expires_at is null or expires_at < now()")
