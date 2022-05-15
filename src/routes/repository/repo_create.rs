cbc0d5d7dc53896b361eb2e943b24325216f09bd
Mari <git@cutegirl.tech>
2022-05-15 15:20:45 +0200

    Hide link to repo importing on creation page if not connected to workhorse

--- src/routes/repository/repo_create.rs
+++ src/routes/repository/repo_create.rs
@@ -1,20 +1,23 @@
 use crate::prelude::ContextExtensions;
-use crate::render_template;
+use crate::{Ipc, render_template};
 use crate::user::WebUser;
 
 use actix_web::{Responder, web};
 use anyhow::Result;
+use futures_locks::RwLock;
 use gitarena_macros::route;
 use sqlx::PgPool;
 use tera::Context;
 
 #[route("/new", method = "GET", err = "html")]
-pub(crate) async fn new_repo(web_user: WebUser, db_pool: web::Data<PgPool>) -> Result<impl Responder> {
+pub(crate) async fn new_repo(web_user: WebUser, ipc: web::Data<RwLock<Ipc>>, db_pool: web::Data<PgPool>) -> Result<impl Responder> {
     let user = web_user.into_user()?;
     let mut transaction = db_pool.begin().await?;
 
     let mut context = Context::new();
     context.insert_user(&user)?;
+
+    context.try_insert("ipc_enabled", &ipc.read().await.is_connected())?;
 
     render_template!("repo/create.html", context, transaction)
 }
