9c0523a6d3ff0a4ba0fbeffb8dd6faa40ae7679a
Mari <git@cutegirl.tech>
2025-11-16 07:20:05 +0100

    upgrade gitarena-ssh also to new sqlx

--- gitarena-ssh/src/keys.rs
+++ gitarena-ssh/src/keys.rs
@@ -2,14 +2,15 @@ use anyhow::Result;
 use futures::TryStreamExt;
 use gitarena_common::database::Database;
 use gitarena_common::database::models::KeyType;
+use gitarena_common::prelude::sqlx::Transaction;
 use gitarena_common::prelude::*;
-use sqlx::{Executor, Row};
+use sqlx::Row;
 
-pub(crate) async fn print_all<'e, E: Executor<'e, Database = Database>>(executor: E) -> Result<()> {
+pub(crate) async fn print_all(tx: &mut Transaction<'_, Database>) -> Result<()> {
     let mut stream = sqlx::query(
         "select algorithm, key from ssh_keys where expires_at is null or expires_at < now()",
     )
-    .fetch(executor);
+    .fetch(&mut **tx);
 
     while let Some(row) = stream.try_next().await? {
         let algorithm: KeyType = row.try_get("algorithm")?;
