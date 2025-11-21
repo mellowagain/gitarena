510508f10cab79579398532e3c664c5ab0c6bf10
Mari <git@cutegirl.tech>
2025-11-21 19:10:45 +0100

    add ssh options

--- gitarena-ssh/src/keys.rs
+++ gitarena-ssh/src/keys.rs
@@ -7,13 +7,17 @@ use gitarena_common::prelude::*;
 use sqlx::Row;
 
 pub(crate) async fn print_all(tx: &mut Transaction<'_, Database>) -> Result<()> {
-    let mut stream = sqlx::query("select algorithm, key from ssh_keys where expires_at is null or expires_at < now()").fetch(&mut **tx);
+    let mut stream = sqlx::query("select algorithm, key, owner from ssh_keys where expires_at is null or expires_at < now()").fetch(&mut **tx);
 
     while let Some(row) = stream.try_next().await? {
         let algorithm: KeyType = row.try_get("algorithm")?;
-        let key: &[u8] = row.try_get("key")?;
 
-        println!("{} {}", algorithm, base64::encode(key));
+        let key: &[u8] = row.try_get("key")?;
+        let encoded = base64::encode(key);
+
+        let owner_id: i32 = row.try_get("owner")?;
+
+        println!("no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty {algorithm} {encoded} {owner_id}");
     }
 
     Ok(())
