35a43fd5456928aed0ff467c8b589054c3e0f3ad
Mari <me@mari.zip>
2026-06-08 23:27:56 +0000

    trim suffix to prevent endless re-plans

--- infra/dns.tf
+++ infra/dns.tf
@@ -11,7 +11,7 @@ resource "cloudflare_dns_record" "frontend" {
   zone_id = var.cloudflare_zone_id
   name    = var.frontend_domain
   type    = "CNAME"
-  content = data.vercel_domain_config.frontend.recommended_cname
+  content = trimsuffix(data.vercel_domain_config.frontend.recommended_cname, ".")
   proxied = false
   ttl     = 600
 }
